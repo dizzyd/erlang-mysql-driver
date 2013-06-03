eecc4076727ea1559deee32bb1c7eae2d9a6a8e9
zhengxujin <zhengxujin@adsage.com>
2013-06-03 17:08:43 +0800

    merged bugfix from other commiters https://github.com/dizzyd/erlang-mysql-driver/pull/19 https://github.com/dizzyd/erlang-mysql-driver/pull/12

--- src/mysql.erl
+++ src/mysql.erl
@@ -825,6 +825,8 @@ encode(Val, true) when is_binary(Val) ->
     quote(Val);
 encode(Val, true) ->
     list_to_binary(encode(Val,false));
+encode(Val, false) when is_boolean(Val) ->
+    atom_to_list(Val);
 encode(Val, false) when is_atom(Val) ->
     quote(atom_to_list(Val));
 encode(Val, false) when is_list(Val) ->
