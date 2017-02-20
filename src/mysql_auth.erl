dizzyd/erlang-mysql-driver

commit d879af8e5afc9a13ae8e2006299b175289caaa07

Update mysql_auth.erl
fix depricated sha_* functions in R16B02

src/mysql_auth.erl
@@ -7,7 +7,7 @@
 %%% Note    : All MySQL code was written by Magnus Ahltorp, originally
 %%%           in the file mysql.erl - I just moved it here.
 %%%
-%%% Copyright (c) 2001-2004 Kungliga Tekniska Högskolan
+%%% Copyright (c) 2001-2004 Kungliga Tekniska Hï¿½gskolan
 %%% See the file COPYING
 %%%
 %%%-------------------------------------------------------------------
@@ -175,11 +175,11 @@ bxor_binary(B1, B2) ->
 password_new([], _Salt) ->
     <<>>;
 password_new(Password, Salt) ->
-    Stage1 = crypto:sha(Password),
-    Stage2 = crypto:sha(Stage1),
-    Res = crypto:sha_final(
-	    crypto:sha_update(
-	      crypto:sha_update(crypto:sha_init(), Salt),
+    Stage1 = crypto:hash(sha, Password),
+    Stage2 = crypto:hash(sha, Stage1),
+    Res = crypto:hash_final(
+	    crypto:hash_update(
+	      crypto:hash_update(crypto:hash_init(sha), Salt),
 	      Stage2)
 	   ),
     bxor_binary(Res, Stage1).

commit b54c4a77faa49efad324d02fd08ce63b80711a4e

Fix copyright comment encoding broken by d879af8

src/mysql_auth.erl
@@ -8,7 +8,7 @@
 %%% Note    : All MySQL code was written by Magnus Ahltorp, originally
 %%%           in the file mysql.erl - I just moved it here.
 %%%
-%%% Copyright (c) 2001-2004 Kungliga Tekniska Hï¿½gskolan
+%%% Copyright (c) 2001-2004 Kungliga Tekniska Högskolan
 %%% See the file COPYING
 %%%
 %%%-------------------------------------------------------------------